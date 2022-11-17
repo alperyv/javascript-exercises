1ac6deb84d4516639616e5296dc5329712ed53fe
alpyavuz0 <alpyvz00@gmail.com>
2022-11-17 13:58:45 +0300

    Master | To Do eklendi

--- ToDo List/to-do.js
+++
@@ -1,9 +0,0 @@
-let input = document.querySelector('#taskInput')
-let addButton = document.querySelector('#addOn')
-let buttons = document.getElementsByClassName('buttonLi')
-let deleteButton = document.querySelectorAll('.buttonDelete')
-
-
-function completed() {
-    buttons.classList.toggle('completedClass')
-}
